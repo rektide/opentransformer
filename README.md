c7cf27deb761c9dd471164e2afa889334dfa4b04
soci-viii <fowle@socialtables.com>
2012-02-08 16:26:55 -0500

    First pass at Open:Transformer: concatenation macros, transformer_node outline.

---
+++ README.md
@@ -0,0 +1,14 @@
+== Open:Transformer ==
+
+Open:Transformer provides helpers for composing CSS Transforms. Like old-school OpenGL, transform's all about building matrix transforming stacks; unlike OpenGL, the API is a big text string.
+
+At it's most basic usage, Transformer provides programmatic constructs for building individual stack elements.
+
+Further, transformer provides a very basic `transform_node` construct for :
+
+a. representing a matrix transformation stack or sub-stack,
+a. combining a heirarchy of nodes into an aggregate transformation stack.
+
+== TODO ==
+
+A chaining API would be swell.
---
+++ transformer.js
@@ -0,0 +1,62 @@
+var transform_node= export.transform_node= function(parent){
+	if(!(this instanceof transform_node))
+		return Object.create(transform_node
+	this.transforms= []
+	this.parent= parent||null
+}
+
+
+// 2D string templates http://www.w3.org/TR/css3-2d-transforms/#transform-functions
+var templates2d= {
+	matrix: ["matrix(",null,",",null,",",null,",", null,",",null,",",null,") ")],
+	translate: ["translate(",null,",",null,") "],
+	translateX: ["translateX(",null,") "],
+	translateY: ["translateY(",null,") "],
+	scale: ["scale(",null,",",null,") "],
+	scaleX: ["scaleX(",null,",",null,") "],
+	scaleY: ["scaleY(",null,",",null,") "],
+	rotate: ["rotate(",null,") "],
+	skewX: ["skewX(",null,") "],
+	skewY: ["skewY(",null,") "] }
+
+// 3D string templates http://www.w3.org/TR/css3-3d-transforms/#transform-functions
+var templates3d= {
+	matrix3d: ["matrix3d(",null,",",null,",",null,",",null,",",
+		null,",",null,",",null,",",null,",",
+		null,",",null,",",null,",",null,",",
+		null,",",null,",",null,",",null,") "],
+	translate3d: ["translate3d(",null,",",null,",",null,") "],
+	translateZ: ["translateZ(",null,") "],
+	scale3d: ["scale3d(",null,",",null,",",null,") "],
+	scaleZ: ["scaleZ(",null,",",null,",",null,") "],
+	rotate3d: ["rotate3d(",null,",",null,",",null,",",null,")" ],
+	rotateZ: ["rotateZ(",null,") "],
+	perspective: ["perspective(",null,") "] }
+
+}
+
+var scaleNaturalToTemplate= [],
+  scaleTemplateToNatural= [],
+  maxArgs= 16 
+for(var i= 0; i< maxArgs; ++i){
+	scaleNaturalToTemplate[i]= 2*i+1 // your index MAY be prime
+	                                 // parents, do you know where your index is?
+	scaleTemplateToNatural[i]= (i+1)/2
+}
+
+function _template(form,args){
+	return function(){
+		for(var i= 0; i< args; ++i)
+			form[scaleNaturalToTemplate[i]] = arguments[i]
+		return form.join("")
+	}
+}
+
+for(var i in templates2d){
+	var template= templates2d[i]
+	exports[i]= _template(template,scaleTemplateToNatural[Math.floor(template.length))
+}
+for(var i in templates3d){
+	var template= templates3d[i]
+	exports[i]= _template(template,scaleTemplateToNatural[Math.floor(template.length))
+}
